71c481aac8959cf83eea9c4a4427779563cd80de
sebe324 <sebeq05@gmail.com>
2022-10-21 10:24:40 +0200

    elo

--- src/main/java/com/sklep/sklepapi/controllers/UserController.java
+++ src/main/java/com/sklep/sklepapi/controllers/UserController.java
@@ -3,25 +3,41 @@ package com.sklep.sklepapi.controllers;
 import com.sklep.sklepapi.repositories.UserRepository;
 import com.sklep.sklepapi.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
 
 @RestController
 public class UserController {
     @Autowired
     UserRepository userRepository;
-    @GetMapping("/")
+    @RequestMapping("/hello")
     public String main(){
-        return "Strona Glowna";
-    }
-    @GetMapping("/api/liczba")
-    public int num(){
-        return 420;
+        return "/WEB-INF/views/demo.jsp";
     }
     @GetMapping("/api/users")
     public List<User> showUsers(){
         return userRepository.getUsers();
+    }
+    @GetMapping("/api/users/{id}")
+    public User showUserById(@PathVariable("id") int id){
+        return userRepository.getUserById(id);
+    }
+    @PostMapping("/api/users/add")
+    public int addUsers(@RequestBody List<User> users){
+        return userRepository.addUsers(users);
+    }
+    @GetMapping("/api/users/update")
+    public String putUser(){
+        return "TODO update-PUT";
+    }
+    @GetMapping("/api/users/patch")
+    public String patchUser(){
+        return "TODO update-patch";
+    }
+    @DeleteMapping("/api/users/delete/{id}")
+    public void deleteUserById(@PathVariable("id") int id){
+        userRepository.deleteUser(id);
     }
 }
--- src/main/java/com/sklep/sklepapi/repositories/UserRepository.java
+++ src/main/java/com/sklep/sklepapi/repositories/UserRepository.java
@@ -16,5 +16,22 @@ public class UserRepository {
     public List<User> getUsers(){
         return jdbcTemplate.query("SELECT * FROM users", BeanPropertyRowMapper.newInstance(User.class));
     }
-
+    public User getUserById(int id){
+        return jdbcTemplate.queryForObject("SELECT * FROM users WHERE id=?", BeanPropertyRowMapper.newInstance(User.class), id);
+    }
+    public int addUsers(List<User> users){
+        int i;
+        for(i=0; i<users.size(); i++) {
+            jdbcTemplate.update("INSERT INTO users (name, lname, email, role, password) VALUES (?,?,?,?,?)",
+                    users.get(i).getName(),
+                    users.get(i).getLname(),
+                    users.get(i).getEmail(),
+                    users.get(i).getRole(),
+                    users.get(i).getPassword());
+        }
+        return i;
+    }
+    public void deleteUser(int id){
+        jdbcTemplate.update("DELETE FROM users WHERE id=?",id);
+    }
 }
